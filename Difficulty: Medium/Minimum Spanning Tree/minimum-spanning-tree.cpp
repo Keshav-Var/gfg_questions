c0cc28b49970ebd1fde2690dbced047e89ab2721
Keshav Varshney <147650289+Keshav-Var@users.noreply.github.com>
2025-02-05 17:59:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -5,26 +5,63 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+    static bool comp(vector<int>& v1, vector<int>& v2){
+        return v1[2] < v2[2];
+    }
+    
+    void initSet(vector<int> &parent, vector<int> &rank, int n){
+        for(int i = 0; i < n; i++){
+            rank[i] = 0;
+            parent[i] = i;
+        }
+    }
+    
+    int findParent(int node, vector<int> &parent){
+        if(parent[node] == node)
+        return node;
+        return parent[node] = findParent(parent[node], parent);
+    }
+    
+    void unionByRank(int node1, int node2,vector<int> &parent, vector<int> &rank){
+        if(rank[node1] == rank[node2]){
+            parent[node2] = node1;
+            rank[node1]++;
+        }
+        else if(rank[node1] < rank[node2])
+            parent[node1] = node2;
+        else
+            parent[node2] = node1;
+    }
+    
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[]) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<bool> visited(V, false);
-        int res = 0;
-        pq.push({0, 0});
-        while(!pq.empty()){
-            pair<int, int> it = pq.top();
-            pq.pop();
-            if (visited[it.second]) continue;
-            res = res + it.first;
-            visited[it.second] = true;
-            for(auto u:adj[it.second]){
-                if(!visited[u[0]]){
-                    pq.push({u[1], u[0]});
-                }
+        int cost = 0;
+        vector<vector<int>> edges;
+        
+        //intializing set
+        vector<int> parent(V);
+        vector<int> rank(V);
+        initSet(parent, rank, V);
+        
+        //converting into edges
+        for(int i = 0; i < V; i++){
+            for(auto u:adj[i]){
+                edges.push_back({i, u[0], u[1]});
             }
         }
-        return res;
+        
+        //kruskal's algo
+        sort(edges.begin(), edges.end(), comp);
+        for(auto e:edges){
+            int c1 = findParent(e[0], parent);
+            int c2 = findParent(e[1], parent);
+            if(c1 != c2){
+                unionByRank(c1, c2, parent, rank);
+                cost += e[2];
+            }
+        }
+        return cost;
     }
 };
 
