660e1db775a35dba9d70f5f0550fdd93f451f724
Keshav Varshney <147650289+Keshav-Var@users.noreply.github.com>
2025-03-13 14:19:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        vector<vector<int>> dp(W+1, vector<int>(wt.size()+1, 0));
+        
+        for(int i = 1; i <= W; i++){
+            for(int j = 1; j <= wt.size(); j++){
+                dp[i][j] = dp[i][j-1];
+                if(wt[j-1] <= i)
+                dp[i][j] = max(dp[i][j], val[j-1]+dp[i-wt[j-1]][j-1]);
+            }
+        }
+        
+        return dp[W][wt.size()];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    // Taking total test cases
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    while (testCases--) {
+        // Reading number of items and capacity
+        int numberOfItems, capacity;
+        vector<int> weights, values;
+        string input;
+        int number;
+
+        // Read capacity and number of items
+        getline(cin, input);
+        stringstream ss(input);
+        ss >> capacity;      // The first number is the capacity
+        ss >> numberOfItems; // The second number is the number of items
+
+        // Read values
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            values.push_back(number);
+        }
+
+        // Read weights
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            weights.push_back(number);
+        }
+
+        Solution solution;
+        cout << solution.knapsack(capacity, values, weights) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
